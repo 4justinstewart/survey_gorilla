8f02773da4ee843062a002575d69a9887f0e5e64
laurendorman <apprentice@dbc-chicago-022.local>
2014-05-02 19:56:27 -0500

    added survey, question creation

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,20 +1,52 @@
 $(document).ready(function() {
 
 
+
   $('a').on("click", function(event){
+
     event.preventDefault();
-    console.log(q_form)
-    $('#question_location').append(q_form)
+    if ($('#question_location').has('div').length > 0) {
+      var data = $('#the_q_form').serialize();
 
 
-    $(".delete_question").on("click", function(){
-    console.log("this")
-    $(this).parent().remove();
-    });
 
-    $('.add_poss').on('click', function(){
-      $(this).siblings('form').append("<br><label for='poss'>possible answer:</label><input type='text' name='possibility'>")
-    })
+
+      $.post('/question', data);
+
+
+
+      $('.question_input').remove();
+
+      $('#question_location').append(q_form);
+
+
+    }
+    else {
+      // instantiate survey into data table
+      $.post("/make_survey", function(response){
+        console.log(response)
+        $('#the_q_form').append("<input type='hidden' name='survey_id' value=" + response + ">")
+      })
+       $('#question_location').append(q_form);
+
+
+    }
+
+
+
+
+
+    // $('#question_location').append(q_form)
+
+
+    // $(".delete_question").on("click", function(){
+    // console.log("this")
+    // $(this).parent().remove();
+    // });
+
+    // $('.add_poss').on('click', function(){
+    //   $(this).siblings('form').append(addPossibleResponse)
+    // })
 
   });
 
@@ -38,5 +70,5 @@ $(document).ready(function() {
 
 });
 
-var q_form = "<div class='question_input'> <img class='delete_question' src='/img/small_x.png' /><form method='post' action='#'> <label for='title'>What's your question?</label><input type='text' name='question'><br><label for='poss'>possible answer:</label><input type='text' name='possibility'><br><label for='poss'>possible answer:</label><input type='text' name='possibility'></form><img class='add_poss' src='/img/plus.png'/></div>"
-var add_pos = "<br><label for='poss'>possible answer:</label><input type='text' name='possibility'>"
+var q_form = "<div class='question_input'> <img class='delete_question' src='/img/small_x.png' /><form id='the_q_form' method='post' action='#'><input type='text' name='question' placeholder='What's your question?''><br><input type='text' name='possibility' placeholder='Enter a possible response'><br><input type='text' name='possibility-2' placeholder='Enter a possible response'></form><img class='add_poss' src='/img/plus.png'/></div>"
+var addPossibleResponse = "<br><input type='text' name='possibility' placeholder='Another possible response'>"
